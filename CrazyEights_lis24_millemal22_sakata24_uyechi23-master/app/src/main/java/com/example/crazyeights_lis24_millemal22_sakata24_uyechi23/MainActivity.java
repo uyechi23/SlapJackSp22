a528d3bc1696e78f509f1fce2c667ba312ab194d
millemal22 <94476407+millemal22@users.noreply.github.com>
2022-01-28 15:54:21 -0800

    Add files via upload

---
+++ CrazyEights_lis24_millemal22_sakata24_uyechi23-master/app/src/main/java/com/example/crazyeights_lis24_millemal22_sakata24_uyechi23/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.crazyeights_lis24_millemal22_sakata24_uyechi23;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
