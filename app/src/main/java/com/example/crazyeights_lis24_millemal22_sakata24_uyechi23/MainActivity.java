2f49ca2ff1d669b518238ddf17f8dfc89491f394
Halcyon <uyechi23@up.edu>
2022-02-11 15:50:08 -0800

    02.11.22_15.48 - Updated activity_main.xml and MainActivity.java to show the UI with changes suggested by Nux.

--- app/src/main/java/com/example/crazyeights_lis24_millemal22_sakata24_uyechi23/MainActivity.java
+++ app/src/main/java/com/example/crazyeights_lis24_millemal22_sakata24_uyechi23/MainActivity.java
@@ -1,3 +1,15 @@
+/**
+ * CRAZY EIGHTS - CS301 Semester Project (Spring 2022)
+ *
+ * @author Maliyah Miller
+ * @author Selena Li
+ * @author Tyler Sakata
+ * @author Jake Uyechi
+ *
+ * This project is for SP22 CS301 - Object-Oriented Programming,
+ * simulating a game of Crazy Eights.
+ */
+
 package com.example.crazyeights_lis24_millemal22_sakata24_uyechi23;
 
 import androidx.appcompat.app.AppCompatActivity;
