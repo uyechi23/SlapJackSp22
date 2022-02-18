d128c4b3709540eac4a2e3321156f5e4841400df
Halcyon <uyechi23@up.edu>
2022-02-18 14:50:21 -0800

    J.02.18.22_14.50 - Added Card class

---
+++ app/src/main/java/com/example/crazyeights_lis24_millemal22_sakata24_uyechi23/Card.java
@@ -0,0 +1,51 @@
+package com.example.crazyeights_lis24_millemal22_sakata24_uyechi23;
+
+import androidx.annotation.NonNull;
+
+public class Card {
+
+    // instance variables of card
+    // suit - the suit of the card
+    // face - the face value of the card
+    // value - the point value of the card (used to calculate post-game scores)
+    public String suit;
+    public String face;
+    public int value;
+
+    public Card(String face, String suit){
+        // assign cards
+        this.suit = suit;
+        this.face = face;
+
+        // calculate card value (an integer representing the face value of the card)
+        switch(face){
+            case "Ace": this.value = 1; break;
+            case "King":
+            case "Queen":
+            case "Jack":
+            case "Ten":
+                this.value = 10; break;
+            case "Nine": this.value = 9; break;
+            case "Eight": this.value = 8; break;
+            case "Seven": this.value = 7; break;
+            case "Six": this.value = 6; break;
+            case "Five": this.value = 5; break;
+            case "Four": this.value = 4; break;
+            case "Three": this.value = 3; break;
+            case "Two": this.value = 2; break;
+        }
+    }
+
+    // getter methods
+    public int getValue(){ return this.value; }
+    public String getFace(){ return this.face; }
+    public String getSuit(){ return this.suit; }
+
+    // compare methods - suit and face
+    public boolean matchSuit(Card compare){ return (compare.suit.equals(this.suit)); }
+    public boolean matchFace(Card compare){ return (compare.face.equals(this.face)); }
+
+    // toString card method
+    @NonNull
+    public String toString(){ return "" + this.face + " of " + this.suit; }
+}
