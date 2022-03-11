645bd1cfea52c0929a44daf1f1365935275ed551
Halcyon <uyechi23@up.edu>
2022-03-11 15:03:53 -0800

    J.03.11.22_14.55 - Update MainActivity to show proper layout

--- app/src/main/java/com/example/crazyeights_lis24_millemal22_sakata24_uyechi23/CrazyEights/MainActivity.java
+++ app/src/main/java/com/example/crazyeights_lis24_millemal22_sakata24_uyechi23/CrazyEights/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.simple_gui);
+        setContentView(R.layout.c8gamestatetest);
 
         SurfaceView gui = findViewById(R.id.gameBoard);
     }
