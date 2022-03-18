58745cc3ce5c4354a10deb63195c90b7d10ed4f7
Halcyon <uyechi23@up.edu>
2022-03-18 16:01:57 -0700

    organizing

--- app/src/main/java/com/example/crazyeights_lis24_millemal22_sakata24_uyechi23/CrazyEights/GameBoardController.java
+++ app/src/main/java/com/example/crazyeights_lis24_millemal22_sakata24_uyechi23/CrazyEights/GameBoardController.java
@@ -1,4 +1,4 @@
-package com.example.crazyeights_lis24_millemal22_sakata24_uyechi23;
+package com.example.crazyeights_lis24_millemal22_sakata24_uyechi23.CrazyEights;
 
 import android.view.DragEvent;
 import android.view.MotionEvent;
